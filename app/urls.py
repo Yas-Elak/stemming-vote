e0e816d685bdb8ae846dda137d0e8edcf82202e7
Yas-Elak <elalaoui87@gmail.com>
2021-02-03 21:06:27 +0100

    language changing button

--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,7 @@
 Copyright (c) 2019 - present AppSeed.us
 """
 
-from django.urls import path, re_path
+from django.urls import path, re_path, include
 from app import views
 
 urlpatterns = [
@@ -13,6 +13,7 @@ urlpatterns = [
 
     # Matches any html file
     #re_path(r'^.*\.*', views.pages, name='pages'),
+    path('i18n/', include('django.conf.urls.i18n')),  # Make sure this is present or the set_language url will not be found
 
 ]
 

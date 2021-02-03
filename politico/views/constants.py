125966334d2166e955bfc5fa3661980b6e8ae117
Yas-Elak <elalaoui87@gmail.com>
2021-02-03 22:03:19 +0100

    def to change url of voter chamber page to nl if needed and made a constant of the list of parties

---
+++ politico/views/constants.py
@@ -0,0 +1,3 @@
+PARTIES_LIST_FOR_TEMPLATES = [['CDV', 'CD&V'], ['cdH', 'cdH'], ['Defi', 'Défi'], ['Ecolo-Groen', 'Ecolo-Groen'], ['MR', 'MR'],
+                       ['N-VA', 'N-VA'], ['Open-Vld', 'Open Vld'], ['PS', 'PS'], ['PVDA-PTB', 'PVDA-PTB'],
+                       ['spa', 'sp.a'], ['VB', 'VB'], ['INDEP', 'INDEP']]
--- politico/views/voters.py
+++ politico/views/voters.py
@@ -4,9 +4,10 @@ from django.utils import translation
 
 from ..models import Seance, VotingPoint, Amendement, TotalVote, Parti, Vote, Voter
 import json
+from .constants import *
 
 
-
+#The segment variable is to activate the right menu in the sidebar
 
 def index(request):
     cur_language = translation.get_language()
@@ -15,25 +16,21 @@ def index(request):
 
     # Need this to loop in the HTML as the name of the parties contains character that does not work for ID
     # a small list here saves us hundreds of line in the html
-    parties_list_id = [['CDV', 'CD&V'], ['cdH', 'cdH'], ['Defi', 'Défi'], ['Ecolo-Groen', 'Ecolo-Groen'], ['MR', 'MR'],
-                       ['N-VA', 'N-VA'], ['Open-Vld', 'Open Vld'], ['PS', 'PS'], ['PVDA-PTB', 'PVDA-PTB'],
-                       ['spa', 'sp.a'], ['VB', 'VB'], ['INDEP', 'INDEP']]
+    parties_list_id = PARTIES_LIST_FOR_TEMPLATES
 
     # the idea is to redirect the people on the chamber with the language they are using, so if they use this site with
     # nl, the link for the chamber will be with language=nl
-    if cur_language == 'nl':
-        for voter in voters:
-            if voter.voter_url is not None:
-                voter.voter_url = (voter.voter_url).replace('fr', 'nl')
+    voters = change_url_to_nl(voters)
 
     return render(request, "politico/members.html", {'members': voters,
                                                      'segment': 'membres',
                                                      'parties_list_id': parties_list_id})
 
 
-
 def detail_voter(request, voter_id):
     voter = Voter.objects.get(id=voter_id)
+    #As I need and return a list for this def, I pass a list of len 1 and get the element 0 of the returned list
+    voter = change_url_to_nl([voter])[0]
 
     votes_count = Vote.objects.filter(voter_id=voter.id).count()
     votes = Vote.objects.filter(voter_id=voter.id)
@@ -47,3 +44,18 @@ def detail_voter(request, voter_id):
                                                     'votes_count': votes_count,
                                                     'votes': votes,
                                                     'seances': seances, 'segment': 'membres'})
+
+
+def change_url_to_nl(voters):
+    """
+    As the url in the db is saved for the french site lachambre.be, I change the language argument of the url
+    from fr to nl if needed
+    :param voters: list
+    :return: list
+    """
+    cur_language = translation.get_language()
+    if cur_language == 'nl':
+        for voter in voters:
+            if voter.voter_url is not None:
+                voter.voter_url = (voter.voter_url).replace('fr', 'nl')
+    return voters

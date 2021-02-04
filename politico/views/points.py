0f028a8e93edbe3a95debd5ec739a08081f74804
Yas-Elak <elalaoui87@gmail.com>
2021-02-04 10:24:10 +0100

    Refactoring use list comprehension to save lines

--- politico/views/points.py
+++ politico/views/points.py
@@ -5,179 +5,42 @@ import json
 
 def detail_voting_point(request, seance_id, votingpoint_id):
 
-    #-------------------------F
-
-
-    stack_chart_json = []
-    decisions_by_parti= []
-
-    #I get the seance
-    #I get the total vote
     seance = Seance.objects.get(id=seance_id)
     voting_point = VotingPoint.objects.get(id=votingpoint_id)
-    vp_own_total_vote = TotalVote.objects.filter(voting_point=votingpoint_id, amendement__isnull=True).first()
+
+    # there is a table with the total_vote for each point. I get the total vot of the current point
+    # this could be empty as some point has no vote but only amendement with votes
+    voting_point_total_vote = TotalVote.objects.filter(voting_point=votingpoint_id, amendement__isnull=True).first()
 
     amendements = Amendement.objects.filter(voting_point=votingpoint_id)
 
+    # I declare the var outside the if to please pycharm, or it says it could be not declared I put -1 as it's impossible
+    # for a total and so, it's easy to find if we have a real total of votes or not.
     total = -1
-    #if the voting point has his own vote (sometiems he has amendment with votes but no vote for itself
-    if vp_own_total_vote:
+    #if the voting point has his own vote (sometimes he has amendment with votes but no vote for itself)
+    if voting_point_total_vote:
         #Need the total for the title of the first chart
-        #I'll get the detail of the vote in the django template
-        total = vp_own_total_vote.number_of_oui + vp_own_total_vote.number_of_non + vp_own_total_vote.number_of_abstention
-        query_set_voters = VotingPoint.objects.filter(totalvote=vp_own_total_vote)
-        all_the_votes = Vote.objects.filter(total_vote=vp_own_total_vote)
+        #I'll get the detail of the vote in the django template with the bar chart
+        total = voting_point_total_vote.number_of_oui + voting_point_total_vote.number_of_non + voting_point_total_vote.number_of_abstention
 
-        #start code  for stacked bar charts
-        # a dict with all the parti, can be harcoded because they are few of them
-        votes_dict = {"CD&V":[],
-                      "cdH":[],
-                      "Défi":[],
-                      "Ecolo-Groen": [],
-                      "MR": [],
-                      "N-VA": [],
-                      "Open Vld": [],
-                      "PS": [],
-                      "PDVA-PTB": [],
-                      "sp.a": [],
-                      "VB": [],
-                      "INDEP": []
-                      }
+        #get the list of all the individual vote that has the foreign key of the total vote count
+        all_the_votes = Vote.objects.filter(total_vote=voting_point_total_vote)
 
-        decisions_by_parti = [
-            ['CD&V', [], [], []],
-            ['cdH', [], [], []],
-            ['Défi', [], [], []],
-            ['Ecolo-Groen', [], [], []],
-            ['MR', [], [], []],
-            ['NV-A', [], [], []],
-            ['Open-Vld', [], [], []],
-            ['PS', [], [], []],
-            ['PDVA-PTB', [], [], []],
-            ['sp.a', [], [], []],
-            ['VB', [], [], []],
-            ['INDEP', [], [], []],
-        ]
+        #start code  for stacked bar charts: get a list of parti to iteate through
+        parties_list = Parti.objects.all().values_list('parti_name', flat=True).order_by('parti_name')
+        #then get a dict where you will put the key as parti and a list of vote decision for value
+        votes_dict = {}
+        for parti in parties_list:
+            votes_dict.setdefault(parti, )
+            votes_dict[parti] = []
+            [votes_dict[parti].append(x.vote_decision) if x.voter.voter_parti.parti_name == parti else "" for x in
+             all_the_votes]
+        # I have now something like that {'CD&V': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 'cdH': [0, 0, 0, 0, 0], 'Défi'..
 
-        #Now for each vote of this point, I'll the voter parti name to know in which key I need to put it
-        #Then I append the vote decision 0 = Yes, 1=no, 2=abstention
-        #BUT I also need a list of name for each decison by partis
-        for vote in all_the_votes:
-            if vote.voter.voter_parti.parti_name == 'CD&V':
-                #logic for chart
-                votes_dict["CD&V"].append(vote.vote_decision)
-                #logic for list
-                if vote.vote_decision == 0:
-                    decisions_by_parti[0][1].append(vote.voter)
-                elif vote.vote_decision == 1:
-                    decisions_by_parti[0][2].append(vote.voter)
-                else:
-                    decisions_by_parti[0][3].append(vote.voter)
-            if vote.voter.voter_parti.parti_name == 'cdH':
-                votes_dict["cdH"].append(vote.vote_decision)
-                # logic for list
-                if vote.vote_decision == 0:
-                    decisions_by_parti[1][1].append(vote.voter)
-                elif vote.vote_decision == 1:
-                    decisions_by_parti[1][2].append(vote.voter)
-                else:
-                    decisions_by_parti[1][3].append(vote.voter)
-            if vote.voter.voter_parti.parti_name == 'Défi':
-                votes_dict["Défi"].append(vote.vote_decision)
-                # logic for list
-                if vote.vote_decision == 0:
-                    decisions_by_parti[2][1].append(vote.voter)
-                elif vote.vote_decision == 1:
-                    decisions_by_parti[2][2].append(vote.voter)
-                else:
-                    decisions_by_parti[2][3].append(vote.voter)
-            if vote.voter.voter_parti.parti_name == 'Ecolo-Groen':
-                votes_dict["Ecolo-Groen"].append(vote.vote_decision)
-                # logic for list
-                if vote.vote_decision == 0:
-                    decisions_by_parti[3][1].append(vote.voter)
-                elif vote.vote_decision == 1:
-                    decisions_by_parti[3][2].append(vote.voter)
-                else:
-                    decisions_by_parti[3][3].append(vote.voter)
-            if vote.voter.voter_parti.parti_name == 'MR':
-                votes_dict["MR"].append(vote.vote_decision)
-                # logic for list
-                if vote.vote_decision == 0:
-                    decisions_by_parti[4][1].append(vote.voter)
-                elif vote.vote_decision == 1:
-                    decisions_by_parti[4][2].append(vote.voter)
-                else:
-                    decisions_by_parti[4][3].append(vote.voter)
-            if vote.voter.voter_parti.parti_name == 'N-VA':
-                votes_dict["N-VA"].append(vote.vote_decision)
-                # logic for list
-                if vote.vote_decision == 0:
-                    decisions_by_parti[5][1].append(vote.voter)
-                elif vote.vote_decision == 1:
-                    decisions_by_parti[5][2].append(vote.voter)
-                else:
-                    decisions_by_parti[5][3].append(vote.voter)
-            if vote.voter.voter_parti.parti_name == 'Open Vld':
-                votes_dict["Open Vld"].append(vote.vote_decision)
-                # logic for list
-                if vote.vote_decision == 0:
-                    decisions_by_parti[6][1].append(vote.voter)
-                elif vote.vote_decision == 1:
-                    decisions_by_parti[6][2].append(vote.voter)
-                else:
-                    decisions_by_parti[6][3].append(vote.voter)
-            if vote.voter.voter_parti.parti_name == 'PS':
-                votes_dict["PS"].append(vote.vote_decision)
-                # logic for list
-                if vote.vote_decision == 0:
-                    decisions_by_parti[7][1].append(vote.voter)
-                elif vote.vote_decision == 1:
-                    decisions_by_parti[7][2].append(vote.voter)
-                else:
-                    decisions_by_parti[7][3].append(vote.voter)
-            if vote.voter.voter_parti.parti_name == 'PVDA-PTB':
-                votes_dict["PDVA-PTB"].append(vote.vote_decision)
-                # logic for list
-                if vote.vote_decision == 0:
-                    decisions_by_parti[8][1].append(vote.voter)
-                elif vote.vote_decision == 1:
-                    decisions_by_parti[8][2].append(vote.voter)
-                else:
-                    decisions_by_parti[8][3].append(vote.voter)
-            if vote.voter.voter_parti.parti_name == 'sp.a':
-                votes_dict["sp.a"].append(vote.vote_decision)
-                # logic for list
-                if vote.vote_decision == 0:
-                    decisions_by_parti[9][1].append(vote.voter)
-                elif vote.vote_decision == 1:
-                    decisions_by_parti[9][2].append(vote.voter)
-                else:
-                    decisions_by_parti[9][3].append(vote.voter)
-            if vote.voter.voter_parti.parti_name == 'VB':
-                votes_dict["VB"].append(vote.vote_decision)
-                # logic for list
-                if vote.vote_decision == 0:
-                    decisions_by_parti[10][1].append(vote.voter)
-                elif vote.vote_decision == 1:
-                    decisions_by_parti[10][2].append(vote.voter)
-                else:
-                    decisions_by_parti[10][3].append(vote.voter)
-            if vote.voter.voter_parti.parti_name == 'INDEP':
-                votes_dict["INDEP"].append(vote.vote_decision)
-                # logic for list
-                if vote.vote_decision == 0:
-                    decisions_by_parti[11][1].append(vote.voter)
-                elif vote.vote_decision == 1:
-                    decisions_by_parti[11][2].append(vote.voter)
-                else:
-                    decisions_by_parti[11][3].append(vote.voter)
-
-
-        #jSON is the easiest way to put the data in the chars, THe carts ask for a list of dict
+        # jSON is the easiest way to put the data in the charts, The charts ask for a list of dict
         data_json_stacked_chart_list = []
         for k, votes_by_parti in votes_dict.items():
-            #k will be the parti name
+            # k will be the parti name
             data_json_stacked_chart_list.append(
                 {
                     'y': k,
@@ -188,13 +51,43 @@ def detail_voting_point(request, seance_id, votingpoint_id):
             )
 
         stack_chart_json = json.dumps(data_json_stacked_chart_list)
-        #end code for chart bar stacked
+        # end code for chart bar stacked
+
+
+        decisions_by_parti = [
+            ['CD&V', [], [], []],
+            ['cdH', [], [], []],
+            ['Défi', [], [], []],
+            ['Ecolo-Groen', [], [], []],
+            ['MR', [], [], []],
+            ['N-VA', [], [], []],
+            ['Open Vld', [], [], []],
+            ['PS', [], [], []],
+            ['PVDA-PTB', [], [], []],
+            ['sp.a', [], [], []],
+            ['VB', [], [], []],
+            ['INDEP', [], [], []],
+        ]
+
+        for vote in all_the_votes:
+            parti_index = -1
+            try:
+                parti_index = next(i for i, v in enumerate(decisions_by_parti) if vote.voter.voter_parti.parti_name in v)
+            except:
+                print(vote.voter.voter_parti.parti_name)
+            if vote.vote_decision == 0:
+                decisions_by_parti[parti_index][1].append(vote.voter)
+            elif vote.vote_decision == 1:
+                decisions_by_parti[parti_index][2].append(vote.voter)
+            else:
+                decisions_by_parti[parti_index][3].append(vote.voter)
+
 
     is_amendement = False
 
     return render(request, 'politico/vote.html', {'seance': seance,
                                                   'voting_point': voting_point,
-                                                  'total_vote': vp_own_total_vote,
+                                                  'total_vote': voting_point_total_vote,
                                                   'total': total,
                                                   'stack_chart_json': stack_chart_json,
                                                   'decisions_by_parti': decisions_by_parti,
@@ -213,15 +106,15 @@ def detail_amendement(request, seance_id, votingpoint_id, amendement_id):
     # I get the total vote
     seance = Seance.objects.get(id=seance_id)
     amendement = Amendement.objects.get(id=amendement_id)
-    vp_own_total_vote = TotalVote.objects.filter(amendement=amendement.id).first()
+    voting_point_total_vote = TotalVote.objects.filter(amendement=amendement.id).first()
 
-    print(vp_own_total_vote.number_of_oui)
-    print(vp_own_total_vote.number_of_non)
+    print(voting_point_total_vote.number_of_oui)
+    print(voting_point_total_vote.number_of_non)
 
 
     # Need the total for the title of the first chart
     # I'll get the detail of the vote in the django template
-    total = vp_own_total_vote.number_of_oui + vp_own_total_vote.number_of_non + vp_own_total_vote.number_of_abstention
+    total = voting_point_total_vote.number_of_oui + voting_point_total_vote.number_of_non + voting_point_total_vote.number_of_abstention
     all_the_votes = Vote.objects.filter(amendement=amendement)
 
 
@@ -393,7 +286,7 @@ def detail_amendement(request, seance_id, votingpoint_id, amendement_id):
     print(stack_chart_json)
     return render(request, 'politico/amendement.html', {'seance': seance,
                                                   'amendement': amendement,
-                                                  'total_vote': vp_own_total_vote,
+                                                  'total_vote': voting_point_total_vote,
                                                   'total': total,
                                                   'stack_chart_json': stack_chart_json,
                                                   'decisions_by_parti': decisions_by_parti,

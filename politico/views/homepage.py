9fb9e3419ec1fa3e508a24d5a05a744847a9c546
Yas-Elak <elalaoui87@gmail.com>
2021-02-07 10:03:27 +0100

    homepage design

--- politico/views/homepage.py
+++ politico/views/homepage.py
@@ -10,7 +10,7 @@ def index(request):
 
     #For now I want to display the last session in the front page, but the title of the session is in french and dutch
     #separated by "/" So I need to split it and get one part or another depending on the user language
-    last_sessions = Seance.objects.filter().order_by('-id')[:5]
+    last_sessions = Seance.objects.filter().order_by('-id')[:8]
     for session in last_sessions:
         if cur_language == 'fr':
             session.seance_name = (session.seance_name).split('/')[0]
@@ -20,14 +20,16 @@ def index(request):
     Total_votes_count = TotalVote.objects.all().count()
     total_politiciens = Voter.objects.all().count()
 
+    #tags by popularity
     tags = Tag.objects.annotate(q_count=Count('voting_point')).order_by('-q_count')[:25]
 
-    for t in tags:
-        print(t.name)
+    voting_points_by_count =  VotingPoint.objects.all().order_by('-users_vote_count')[:5]
+
     #Législature en cours: 55
 
     return render(request, "index.html", {'seances': last_sessions,
                                           'total_votes_count': Total_votes_count,
                                           'voters': total_politiciens,
-                                          'tags': tags})
+                                          'tags': tags,
+                                          'voting_points_by_count':voting_points_by_count})
 

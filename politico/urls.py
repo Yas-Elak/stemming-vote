b9a6c98187146fdcb151f2e4200b1ac63c9a405e
Yas-Elak <elalaoui87@gmail.com>
2021-02-07 08:35:12 +0100

    tag moderation

--- politico/urls.py
+++ politico/urls.py
@@ -26,6 +26,9 @@ urlpatterns = [
     path('admin_web/articles/', admin_moderation.index_article, name='admin_mod_articles'),
     path('admin_web/article/<int:article_id>/', admin_moderation.delete_article, name='admin_delete_article'),
     path('admin_web/article/<int:article_id>/<str:lang>/', admin_moderation.add_article, name='admin_add_article'),
+    path('admin_web/tags/', admin_moderation.index_tag, name='admin_mod_tag'),
+    path('admin_web/tag/<int:tag_id>/', admin_moderation.delete_tag, name='admin_delete_tag'),
+    path('admin_web/tag/<int:tag_id>/add/', admin_moderation.add_tag, name='admin_add_tag'),
 
     path('seance/<int:seance_id>/<int:votingpoint_id>/<int:is_amendement>/<int:result>/', points.user_vote, name="voting_point_user_vote"),
 
--- politico/views/admin_moderation.py
+++ politico/views/admin_moderation.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import user_passes_test
-from ..models import ProposedArticle, LinkArticle
+from ..models import ProposedArticle, LinkArticle, Tag
 from django.shortcuts import redirect
 
 @user_passes_test(lambda u: u.is_superuser)
@@ -11,8 +11,31 @@ def index_article(request):
 
     return render(request, "politico/admin_proposed_articles.html", {'segment':segment,
                                                                      'articles': articles
-
     })
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def index_tag(request):
+
+    segment = 'admin_tag'
+    tags = Tag.objects.filter(to_moderate=1)
+
+    return render(request, "politico/admin_proposed_tags.html", {'segment':segment,
+                                                                     'tags': tags
+    })
+
+@user_passes_test(lambda u: u.is_superuser)
+def delete_tag(request,tag_id=None):
+    tag = Tag.objects.get(id=tag_id)
+    tag.delete()
+    return redirect('/admin_web/tags/')
+
+@user_passes_test(lambda u: u.is_superuser)
+def add_tag(request, tag_id):
+    tag = Tag.objects.get(id=tag_id)
+    tag.to_moderate = 0
+    tag.save()
+    return redirect('/admin_web/tags/')
 
 @user_passes_test(lambda u: u.is_superuser)
 def delete_article(request,article_id =None):

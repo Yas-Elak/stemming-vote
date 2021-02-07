57bd82c3ffbf628af5a329304ec720df764ed4e8
Yas-Elak <elalaoui87@gmail.com>
2021-02-07 13:52:11 +0100

    repair search

--- politico/views/search.py
+++ politico/views/search.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.utils import translation
 
 from ..models import Seance, VotingPoint, Amendement, Voter
 from django.db.models import Q # new
@@ -13,19 +14,38 @@ def index(request):
 
 
 def result(request):
+    cur_language = translation.get_language()
     query = request.GET.get('q')
-    seances = Seance.objects.filter(
-        Q(seance_name__icontains=query) | Q(seance_date__icontains=query)
-    ).order_by('-seance_date')
-    voting_points = VotingPoint.objects.filter(
-        Q(point_title_nl__icontains=query) | Q(point_title_fr__icontains=query)
-    )
-    amendements = Amendement.objects.filter(
-        Q(title_nl__icontains=query) | Q(title_fr__icontains=query)
-    )
-    members = Voter.objects.filter(
-        Q(voter_name__icontains=query) | Q(voter_email__icontains=query)
-    ).order_by('voter_parti')
+
+    if cur_language == 'fr':
+        seances = Seance.objects.filter(
+            Q(seance_name__icontains=query) | Q(seance_date__icontains=query)
+        ).order_by('-seance_date')
+
+        voting_points = VotingPoint.objects.filter(
+            Q(point_title_fr__icontains=query)
+        )
+
+        amendements = Amendement.objects.filter(
+            Q(title_fr__icontains=query)
+        )
+        members = Voter.objects.filter(
+            Q(voter_name__icontains=query) | Q(voter_email__icontains=query)
+        ).order_by('voter_parti')
+    else:
+
+        seances = Seance.objects.filter(
+            Q(seance_name__icontains=query) | Q(seance_date__icontains=query)
+        ).order_by('-seance_date')
+        voting_points = VotingPoint.objects.filter(
+            Q(point_title_nl__icontains=query)
+        )
+        amendements = Amendement.objects.filter(
+            Q(title_nl__icontains=query)
+        )
+        members = Voter.objects.filter(
+            Q(voter_name__icontains=query) | Q(voter_email__icontains=query)
+        ).order_by('voter_parti')
 
     return render(request, "politico/search_result.html", {'seances': seances,
                                                            'voting_points': voting_points,
